9324dfe2581879a0c6c50bb45bc7c7e5a1869747
Toxicoman9991 <saniazenin@yandex.ru>
2024-06-19 12:21:16 +0300

    Нашёл похожую проблему, как в коде отправки запроса, что можно менять поля на не-кириллические символы. Исправлено.

--- src/scripts/php/update_request.php
+++ src/scripts/php/update_request.php
@@ -3,7 +3,7 @@ session_start();
 require_once('database.php');
 require_once('check_request_status.php');
 
-// Проверить, авторизован ли пользователь
+// Проверяем, авторизован ли пользователь
 if (!isset($_SESSION['user'])) {
     // Если пользователь не авторизован, перенаправляем его на страницу входа
     header('Location: /phprequest/index.php');
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['requests_id']) && !is
     $snils = substr($snils, 0, 11); // Ограничиваем до 11 символов
     $lastName = pg_escape_string(databaseConnection(), $_POST['last_name_citizen']);
     $firstName = pg_escape_string(databaseConnection(), $_POST['first_name_citizen']);
-    $middleName = isset($_POST['middle_name_citizen']) ? pg_escape_string(databaseConnection(), $_POST['middle_name_citizen']) : null;
+    $middleName = !empty($_POST['middle_name_citizen']) ? pg_escape_string(databaseConnection(), $_POST['middle_name_citizen']) : null;
     $birthday = pg_escape_string(databaseConnection(), $_POST['birthday_citizen']);
     $startDate = pg_escape_string(databaseConnection(), $_POST['requested_date_start']);
     $endDate = pg_escape_string(databaseConnection(), $_POST['requested_date_end']);
@@ -67,6 +67,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['requests_id']) && !is
         $errors[] = 'Некорректный запрашиваемый период. Убедитесь, что вы ввели даты в правильном формате и дата окончания больше даты начала.';
     }
 
+    // Проверка на кириллические символы в фамилии, имени и отчестве
+    if (!preg_match('/^[а-яёА-ЯЁ\s-]+$/u', $lastName)) {
+        $errors[] = 'Фамилия должна содержать только кириллические символы, пробелы и дефисы.';
+    }
+    if (!preg_match('/^[а-яёА-ЯЁ\s-]+$/u', $firstName)) {
+        $errors[] = 'Имя должно содержать только кириллические символы, пробелы и дефисы.';
+    }
+    if (!empty($middleName) && !preg_match('/^[а-яёА-ЯЁ\s-]*$/u', $middleName)) {
+        $errors[] = 'Отчество должно содержать только кириллические символы, пробелы и дефисы.';
+    }
+
     // Если есть ошибки, объединяем их в одну строку
     if (!empty($errors)) {
         $_SESSION['edit_request_error'] = implode("<br>", $errors);
@@ -89,13 +100,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['requests_id']) && !is
         }
     }
 
-    // Если файл не был удален или не было ошибок, обновляем данные запроса с возможной загрузкой файла
-    // Тогда выполняем запрос на обновление данных запроса
+    // Преобразование middleName в NULL, если оно пустое
+    $middleNameForQuery = $middleName ? "'$middleName'" : "NULL";
+
+    // Выполняем запрос на обновление данных запроса
     $query = "UPDATE phprequest_schema.requests 
         SET snils_citizen = '$snils',
             last_name_citizen = '$lastName',
             first_name_citizen = '$firstName',
-            middle_name_citizen = '$middleName',
+            middle_name_citizen = $middleNameForQuery,
             birthday_citizen = '$birthday',
             requested_date_start = '$startDate',
             requested_date_end = '$endDate',
@@ -110,85 +123,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['requests_id']) && !is
             $uploadDir = '/phprequest/documents/'; // Каталог для сохранения загруженных файлов
             $uploadFile = $uploadDir . basename($_FILES['file_upload']['name']);
 
-            // Перемещаем загруженный файл в указанную директорию
-            if(move_uploaded_file($_FILES['file_upload']['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . $uploadFile)) {
-                // Обновляем запись в базе данных с путем к загруженному файлу
-                $queryUpdateFile = "UPDATE phprequest_schema.requests SET download_link = '$uploadFile' WHERE requests_id = '$requestId'";
-                $resultUpdateFile = pg_query(databaseConnection(), $queryUpdateFile);
-
-                if(!$resultUpdateFile) {
-                    $_SESSION['edit_request_error'] = 'Ошибка при обновлении пути к файлу';
-                    header('Location: /phprequest/src/scripts/php/edit_request.php?requests_id=' . $requestId);
-                    exit();
-                }
-            } else {
-                $_SESSION['edit_request_error'] = 'Ошибка при загрузке файла';
-                header('Location: /phprequest/src/scripts/php/edit_request.php?requests_id=' . $requestId);
-                exit();
+            // Перемещаем загруженный файл в указанный каталог
+            if (move_uploaded_file($_FILES['file_upload']['tmp_name'], $uploadFile)) {
+                // Обновляем ссылку на загруженный файл в базе данных
+                $updateFileQuery = "UPDATE phprequest_schema.requests 
+                                    SET download_link = '$uploadFile'
+                                    WHERE requests_id = '$requestId'";
+                pg_query(databaseConnection(), $updateFileQuery);
             }
         }
 
-        // Проверяем, если статус запроса был установлен в 3 (Ответ по запросу получен и обрабатывается),
-        // то устанавливаем статус 4 (Ответ обработан. Запрос закрыт), если файл был загружен
-        if ($requestStatusId == 3) {
-            // Получаем ссылку на файл из базы данных
-            $queryGetFile = "SELECT download_link FROM phprequest_schema.requests WHERE requests_id = '$requestId'";
-            $resultGetFile = pg_query(databaseConnection(), $queryGetFile);
-            if ($resultGetFile && pg_num_rows($resultGetFile) > 0) {
-                $row = pg_fetch_assoc($resultGetFile);
-                if ($row['download_link'] !== null) {
-                    $queryUpdateStatus = "UPDATE phprequest_schema.requests SET request_status_id = 4 WHERE requests_id = '$requestId'";
-                    $resultUpdateStatus = pg_query(databaseConnection(), $queryUpdateStatus);
-                    if (!$resultUpdateStatus) {
-                        $_SESSION['edit_request_error'] = 'Ошибка при обновлении статуса запроса';
-                        header('Location: /phprequest/src/scripts/php/edit_request.php?requests_id=' . $requestId);
-                        exit();
-                    }
-                }
-            }
-        }
-        $_SESSION['edit_request_success'] = 'Данные запроса успешно обновлены';
+        $_SESSION['edit_request_success'] = 'Запрос успешно обновлён.';
     } else {
-        $_SESSION['edit_request_error'] = 'Ошибка при обновлении данных запроса' .pg_last_error(databaseConnection());
+        $_SESSION['edit_request_error'] = 'Произошла ошибка при обновлении запроса.';
     }
+    header('Location: /phprequest/src/scripts/php/edit_request.php?requests_id=' . $requestId);
+    exit();
 }
 
-// Проверить, был ли отправлен POST-запрос для удаления файла
-else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['requests_id']) && isset($_POST['delete_file'])) {
-    $requestId = pg_escape_string(databaseConnection(), $_POST['requests_id']);
-
-    // Получить ссылку на файл из базы данных
-    $queryGetFile = "SELECT download_link FROM phprequest_schema.requests WHERE requests_id = '$requestId'";
-    $resultGetFile = pg_query(databaseConnection(), $queryGetFile);
-    if ($resultGetFile && pg_num_rows($resultGetFile) > 0) {
-        $row = pg_fetch_assoc($resultGetFile);
-        $filePath = $_SERVER['DOCUMENT_ROOT'] . $row['download_link'];
-
-        // Удалить файл, если он существует
-        if (file_exists($filePath)) {
-            if (unlink($filePath)) {
-                $_SESSION['edit_request_success'] = 'Файл успешно удален';
-            } else {
-                $_SESSION['edit_request_error'] = 'Ошибка при удалении файла';
-            }
-        } else {
-            $_SESSION['edit_request_error'] = 'Файл не найден';
-        }
-    } else {
-        $_SESSION['edit_request_error'] = 'Ошибка при получении ссылки на файл';
-    }
-
-
-    // Удалить ссылку на файл из базы данных
-    $queryDeleteFile = "UPDATE phprequest_schema.requests SET download_link = NULL WHERE requests_id = '$requestId'";
-    $resultDeleteFile = pg_query(databaseConnection(), $queryDeleteFile);
-    if (!$resultDeleteFile) {
-        $_SESSION['edit_request_error'] = 'Ошибка при удалении ссылки на файл';
-    } else {
-        $_SESSION['edit_request_success'] = 'Файл успешно удален';
-    }
-}
-
-// Редирект на страницу, откуда была отправлена форма
-header('Location: /phprequest/src/scripts/php/edit_request.php?requests_id=' . $requestId);
+// Если скрипт дошел до этой точки, значит что-то пошло не так или пользователь не имеет прав на редактирование запроса
+header('Location: /phprequest/src/pages/main.php');
 exit();
